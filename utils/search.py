8c15779aa78bd465f5002aba507b534f3e14f766
BarryVanAllen <theo6740@hotmail.fr>
2024-12-26 18:23:15 +0100

    Update search.py
    avec les specs

--- utils/search.py
+++ utils/search.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+
 import operator
 
 class SearchEngine:
@@ -6,10 +6,27 @@ class SearchEngine:
         self.data = data
 
     def search(self, field, value):
+        """Recherche dans la liste de dictionnaire data.
+
+        PRE: self.data est une liste de dictionnaire.
+            field est un champ dans les dictionnaires.
+            value doit être une chaîne à rechercher dans le champ spécifié.
+
+        POST : renvoie une liste de ligne ou les champs spécifié field correspond exactement à value.
+
+        """
         return [row for row in self.data if row.get(field, '').lower() == value.lower()]
 
     def advanced_search(self, field, operator_symbol, value):
-        """Recherche avancée avec des opérateurs (>, <, ==, etc.)."""
+        """Recherche avancée avec des opérateurs (>, <, ==, etc.).
+        PRE: self.data est une liste de dictionnaire.
+                field est un champ dans les dictionnaires.
+                operator_symbol est opérateur valide.
+
+        POST : Retourne une liste de lignes où le champ field est conforme au triage de l'opérateur.
+        RAISE : ValueError : si l'opérateur est invalide ou si la conversion du value en numérique échoue
+
+        """
         OPERATORS = {
             "<": operator.lt,
             ">": operator.gt,
